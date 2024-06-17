753a2973eb6b8161a34497735648c66f4152204e
khangcntt2002 <minhkhang220602@gmail.com>
2024-06-17 20:37:01 +0700

    khang

--- CPP_Bancuuchuong.cpp
+++ CPP_Bancuuchuong.cpp
@@ -1,6 +1,18 @@
-// CPP_Bancuuchuong.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
+{
+    int number;
+
+    cout << "Nhap So ";
+    cin >> number;
+
+    cout << "Bang Cuu Chuong " << number << ":\n";
+    for (int i = 1; i <= 10; ++i)
+    {
+        cout << number << " x " << i << " = " << (number * i) << "\n";
+    }
+
+    return 0;
+}
